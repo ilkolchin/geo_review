59a2bece8df35fdd46fb3be5f99f2321f2311686
Ilia Kolchin <wheezyseal@gmail.com>
2021-08-20 02:45:15 +0300

    added: placemark, localstorage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./css/main.css"
 import { initMap } from "./js/ymaps";
-import { initForm } from "./js/form";
+// import { initForm } from "./js/form";
 
 
 window.onload = initMap()
-window.onload = initForm()
+// window.onload = initForm()
--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,12 +1,56 @@
+import { initForm } from "./form";
+
 function initMap() {
-    ymaps.ready(() =>{
-        let myMap = new ymaps.Map("map", {
-            center: [59.938,30.3],
-            zoom: 12.4
-        })
-    })
+
+	ymaps.ready(() => {
+
+		var myMap = new ymaps.Map("map", {
+			center: [59.938, 30.3],
+			zoom: 12.4
+		});
+
+		initForm();
+
+
+		myMap.events.add('click', (e) => {
+			let coords = e.get('coords');
+			let myPlacemark = new ymaps.Placemark(coords);
+			myMap.geoObjects.add(myPlacemark);
+		});
+
+		// var placemark = new ymaps.Placemark(myMap.getCenter(), {
+		//   // Зададим содержимое заголовка балуна.
+		//   // balloonContentHeader: '<a href = "#">Рога и копыта</a><br>' +
+		//   //   '<span class="description">Сеть кинотеатров</span>',
+		//   // Зададим содержимое основной части балуна.
+		//   balloonContentBody: '<div class="review">'+
+		//   '<div class="review__inner">'+
+		//     '<div class="review__previous"></div>'+
+		//     '<div class="review__form">'+
+		//     '<h1 class="review__header">Отзыв:</h1>'+
+		//     '<div class="review__inputs">'+
+		//       '<input type="text" placeholder="Укажите ваше имя" class="review__input">'+
+		//       '<input type="text" placeholder="Укажите место" class="review__input">'+
+		//       '<input type="text" placeholder="Оставьте отзыв" class="review__text">'+
+		//     '</div>'+
+		//     '<button class="btn">Добавить</button>'+
+		//     '</div>'+
+		//   '</div>'+
+		//   '</div>',
+		//   // Зададим содержимое нижней части балуна.
+		//   // balloonContentFooter: 'Информация предоставлена:<br/>OOO "Рога и копыта"',
+		//   // Зададим содержимое всплывающей подсказки.
+		//   // hintContent: 'Рога и копыта'
+		// });
+		// // Добавим метку на карту.
+		// myMap.geoObjects.add(placemark);
+		// // Откроем балун на метке.
+		// // placemark.balloon.open();
+
+
+	})
 }
 
 export {
-    initMap
+	initMap
 }
